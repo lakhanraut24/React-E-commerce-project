39be2a0ce4b79411c904a20f0c895d70da87d37f
lakhan raut <lakhanraut24@gmail.com>
2022-10-19 19:18:25 +0530

    changes in add_to_cart branch --cart item 0--

--- Furns-Ecom/src/Components/Header/Navbar.jsx
+++ Furns-Ecom/src/Components/Header/Navbar.jsx
@@ -54,13 +54,14 @@ const Navbar = () => {
               <div className="hidden xl:flex items-center space-x-5 items-center mr-4">
                 
                 <a className="flex items-center hover:text-gray-200" href="#">
-                <span className="font-bold text-xl">cart (0)</span>
+                <span className="font-bold text-xl">cart <span>(0)</span></span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
+                    
                   >
                     <path
                       stroke-linecap="round"
